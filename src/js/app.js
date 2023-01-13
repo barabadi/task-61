be70d176d8ca315643eacbdfc05998c3b8203188
barabadi <101886037+barabadi@users.noreply.github.com>
2023-01-13 16:05:09 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,10 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 });
+const prices = document.querySelectorAll(".price");
+
+prices.forEach(price => {
+  if (price.classList.contains("hot")) {
+    price.insertAdjacentHTML("beforeend", "🔥");
+  }
+});
